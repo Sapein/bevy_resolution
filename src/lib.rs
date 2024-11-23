7a62a85256c156102fba47f7bf90926c9fcd30e1
Sapein <5852983+Sapein@users.noreply.github.com>
2024-11-23 10:12:35 -0600

    Update readme and add in some crate-level documentation
    I've updated Readme to include some additional information, and to have badges.
    
    I've also updated `lib.rs` to have some documentation at the module/crate level so it's a bit easier to understand.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,20 @@
+//! `bevy_resolution` aims to provide a simple, easy, and convenient way to set and manage window
+//! resolutions. It provides conveniences for commonly-supported Window Resolutions *and* also
+//! provides a way to manage window resolution scaling for arbitrary resolutions.
+//! 
+//! This crate requires bevy version `0.15.0-rc.3`, as it relies on features introduced in the
+//! release candidate.
+//!
+//! ## Features
+//! - Provides an easy, but powerful, API for resolution management
+//! - Provides convenience wrappers for common 16:9 and 4:3 resolutions
+//! - Can support any resolution with any Aspect Ratio and uses Bevy's built-in `AspectRatio`
+//! 
+//! ## Feature Flags
+//! This crate exposes the following feature flags:
+//! - `bevy_window`: Enables support for usage with `bevy_window`, this is required for usage with `bevy_window::WindowResolution`
+//! 
+//! The `bevy_window` feature is enabled by default.
+//! 
 pub mod common;
 pub mod resolutions;
